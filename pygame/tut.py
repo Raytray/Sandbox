4601aa09b213abc9999da40cea960f1cff1edc28
Raymond Tang <Raytray25@gmail.com>
2013-03-11 22:06:42 -0400

    Created a trail using trail.png. Strange overlaps though.

--- pygame/tut.py
+++ pygame/tut.py
@@ -8,20 +8,22 @@ black = 0, 0, 0
 
 screen = pygame.display.set_mode(size)
 
+trail = pygame.image.load("trail.png")
+trailrect = trail.get_rect()
 ball = pygame.image.load("ball.png")
 ballrect = ball.get_rect()
 
-ballrect = ballrect.move(speed)
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
 
+    screen.blit(trail, trailrect)
     ballrect = ballrect.move(speed)
     if ballrect.left < 0 or ballrect.right > width:
         speed[0] = -speed[0]
     if ballrect.top < 0 or ballrect.bottom > height:
         speed[1] = -speed[1]
 
-    screen.fill(black)
     screen.blit(ball, ballrect)
+    trailrect = trailrect.move(speed)
     pygame.display.flip()
