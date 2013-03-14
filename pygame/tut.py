7a2c3102051be756e17a52ad0afc5e27c2fb9d70
Raymond Tang <Raytray25@gmail.com>
2013-03-14 05:23:21 -0400

    Fixed red trail being oddly done. Should use an array to keep track of where the red trail is instead of exploiting where the screen updates. This would enable us to calculate into the future.

--- pygame/tut.py
+++ pygame/tut.py
@@ -9,21 +9,20 @@ black = 0, 0, 0
 screen = pygame.display.set_mode(size)
 
 trail = pygame.image.load("trail.png")
-trailrect = trail.get_rect()
 ball = pygame.image.load("ball.png")
 ballrect = ball.get_rect()
 
 while 1:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
+        if event.type ==pygame.QUIT:
+            sys.exit()
 
-    screen.blit(trail, trailrect)
+    screen.blit(trail, ballrect)
     ballrect = ballrect.move(speed)
+    screen.blit(ball, ballrect)
     if ballrect.left < 0 or ballrect.right > width:
         speed[0] = -speed[0]
     if ballrect.top < 0 or ballrect.bottom > height:
         speed[1] = -speed[1]
 
-    screen.blit(ball, ballrect)
-    trailrect = trailrect.move(speed)
     pygame.display.flip()
