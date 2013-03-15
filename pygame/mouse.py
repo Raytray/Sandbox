9d0f22556baa110fa4e78085817cc5623b120d1e
Raymond Tang <Raytray25@gmail.com>
2013-03-14 21:37:29 -0400

    Tracking the center of the ball object, will use to create a max speed.

--- pygame/mouse.py
+++ pygame/mouse.py
@@ -10,13 +10,17 @@ screen = pygame.display.set_mode(size)
 trail = pygame.image.load("trail.png")
 ball = pygame.image.load("ball.png")
 ballrect = ball.get_rect()
+mouse_pos = pygame.mouse.get_pos()
+ballrect.center = mouse_pos
 
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-    screen.blit(trail, ballrect)    
+
+    screen.blit(trail, ballrect)
     mouse_pos = pygame.mouse.get_pos()
     ballrect.center = mouse_pos
+    print ballrect.center, mouse_pos
     screen.blit(ball, ballrect)
     pygame.display.flip()
