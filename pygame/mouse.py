6a49815a0c949c215359f08c10135919786e8e34
Raymond Tang <Raytray25@gmail.com>
2013-03-14 21:19:47 -0400

    Ball follows and tracks mouse leaving a trail behind it.

---
+++ pygame/mouse.py
@@ -0,0 +1,22 @@
+import sys
+import pygame
+pygame.init()
+
+size = width, height = 800, 600
+black = 0, 0, 0
+
+screen = pygame.display.set_mode(size)
+
+trail = pygame.image.load("trail.png")
+ball = pygame.image.load("ball.png")
+ballrect = ball.get_rect()
+
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+    screen.blit(trail, ballrect)    
+    mouse_pos = pygame.mouse.get_pos()
+    ballrect.center = mouse_pos
+    screen.blit(ball, ballrect)
+    pygame.display.flip()
