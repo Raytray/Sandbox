b1da780013b7185e301a493f6b1d41b57dc40969
Raymond Tang <Raymond@tang.me>
2013-04-02 09:50:05 -0400

    Threw in a main function.

--- pygame/mouse.py
+++ pygame/mouse.py
@@ -1,27 +1,32 @@
 import sys
 import pygame
-pygame.init()
 
-size = width, height = 800, 600
-black = 0, 0, 0
+def main():
+    pygame.init()
 
-screen = pygame.display.set_mode(size)
+    size = width, height = 800, 600
+    black = 0, 0, 0
 
-trail = pygame.image.load("trail.png")
-ball = pygame.image.load("ball.png")
-ballrect = ball.get_rect()
-mouse_pos = pygame.mouse.get_pos()
-ballrect.center = mouse_pos
+    screen = pygame.display.set_mode(size)
 
-while 1:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
-        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-            screen.fill(black)
-
-    screen.blit(trail, ballrect)
+    trail = pygame.image.load("trail.png")
+    ball = pygame.image.load("ball.png")
+    ballrect = ball.get_rect()
     mouse_pos = pygame.mouse.get_pos()
     ballrect.center = mouse_pos
-    screen.blit(ball, ballrect)
-    pygame.display.flip()
+
+    while 1:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                screen.fill(black)
+
+        screen.blit(trail, ballrect)
+        mouse_pos = pygame.mouse.get_pos()
+        ballrect.center = mouse_pos
+        screen.blit(ball, ballrect)
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
