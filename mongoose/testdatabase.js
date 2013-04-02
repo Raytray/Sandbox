8516e6a5463036788410b6125386d3909237aaa2
Raymond Tang <Raymond@tang.me>
2013-04-02 12:21:49 -0400

    Determine globalness.

--- mongoose/testdatabase.js
+++ mongoose/testdatabase.js
@@ -4,33 +4,35 @@ mongoose.connect('mongodb://localhost/test');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function callback() {
-    var kittySchema = mongoose.Schema({
-	name: String,
-	_id: String
-    })
+    console.log("connected!");
+});
 
-    kittySchema.methods.speak = function () {
-	var greeting = this.name
+var kittySchema = mongoose.Schema({
+    name: String,
+    _id: String
+})
+
+kittySchema.methods.speak = function () {
+    var greeting = this.name
 	? "Meow name is " + this.name
 	: "I don't have a name"
-	console.log(greeting);
+    console.log(greeting);
+}
+var Kitten = mongoose.model('Kitten', kittySchema);
+var fluffy = new Kitten({ name: 'fuffy' });
+fluffy["newVar"] = "awesome";
+fluffy.update(function (err,fluffy){
+    if (err){
+	console.log(err);
     }
-    var Kitten = mongoose.model('Kitten', kittySchema);
-    var fluffy = new Kitten({ name: 'fluffy', _id: "fluffID" });
-    fluffy.speak();
-    fluffy.save(function (err,fluffy){
-	if (err){
-	    console.log(err);
-	}
-	else{
-	    fluffy.speak();
-	}
-    });
+    else{
+	fluffy.speak();
+    }
+});
 
-    Kitten.find(function(err, kittens) {
-	if (err){
-	    console.log(err);
-	}
-	console.log(kittens)
-    });
+Kitten.find(function(err, kittens) {
+    if (err){
+	console.log(err);
+    }
+    console.log(kittens)
 });
